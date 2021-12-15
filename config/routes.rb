d70881295e292a50cdcf88a6379d7967e39f8998
tatm2110 <ts2021ik06@gmail.com>
2021-12-15 06:39:46 +0000

    fix admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  # devise_for :admins
+  # 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+
+  # devise_for :members
+  # 顧客用
+# URL /members/sign_in ...
+devise_for :members, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
